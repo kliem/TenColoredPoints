e30645f9a101b182b038e364fca7bebe4d18ff43
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-06-09 10:32:05 +0200

    remove overalignment and specialized union instructions as it does not give a difference in performance

--- tencoloredpoints/bitset2.h
+++ tencoloredpoints/bitset2.h
@@ -8,14 +8,11 @@ class Bitset2;
 
 class Bitset2 : public Bitset {
     public:
-        Bitset2(int n_vertices);
-        ~Bitset2();
+        Bitset2(int n_vertices) : Bitset(n_vertices) {}
         void operator=(const Bitset2&);
         void clear();
         void union_assign(Bitset2& l, Bitset2& r);
         void flip_inplace();
-    private:
-        uint64_t* mem;
 };
 
 #endif
